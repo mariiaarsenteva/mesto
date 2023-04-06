df391bb500fef036af2b7451a12072dbbcdf49ea
Mariia Arsenteva <mariiaarsenteva@yandex.ru>
2023-04-06 13:00:29 +0300

    работает попап редактирования профиля

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,40 @@
 const popupElement = document.querySelector('.popup');
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-let formElement = popupElement.querySelector('.popup__form');
-let nameInput = formElement.querySelector('#nameInput');
-let jobInput = formElement.querySelector('#jobInput');
-let profileElement = document.querySelector('.profile');
-let profileName = profileElement.querySelector('.profile__name');
-let profileBio = profileElement.querySelector('.profile__bio');
+const profileElement = document.querySelector('.profile');
+const profilePopupElement = document.querySelector('.profile-popup');
+const cardPopupElement = document.querySelector('.card-popup');
+const imagePopupElement = document.querySelector('.image-popup');
+const formEditProfileElement = document.querySelector('.popup__form');
+const formAddCardElement = document.querySelectorAll('.popup__form');
+const popupEditButtonElement = profileElement.querySelector('.profile__edit-button');
+const popupCloseButtonElement = document.querySelector('.popup__close-button');
+const popupAddButtonElement = imagePopupElement.querySelector('.profile__add-button');
+const profileName = profileElement.querySelector('.profile__name');
+const profileJob = profileElement.querySelector('.profile__job');
+const nameInput = document.querySelector('#nameInput');
+const titleInput = document.querySelector('#titleInput');
+const jobInput = document.querySelector('#jobInput');
+const linkInput = document.querySelector('#linkInput');
 
-const openPopup = function () {
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileBio.textContent;
+const cardTemplate = document.querySelector('#cardTemplate').content;
+
+
+
+const openPopup = function (popup) {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
     popupElement.classList.add('popup_opened');
 };
 
-const closePopup = function () {
-    popupElement.classList.remove('popup_opened');
+const closePopup = function (popup) {
+  popupElement.classList.remove('popup_opened');
 };
 
 function handleFormSubmit(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileBio.textContent = jobInput.value;
-    closePopup();
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
 }
-
-formElement.addEventListener('submit', handleFormSubmit);
-
-popupOpenButtonElement.addEventListener('click', openPopup);
+profilePopupElement.addEventListener('submit', handleFormSubmit);
+popupEditButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
