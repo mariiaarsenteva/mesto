0e3d8d6e83ced4ecdaab0c77ddafc3e4e3a77601
Mariia Arsenteva <mariiaarsenteva@yandex.ru>
2023-04-06 20:49:07 +0300

    появились карточки

---
+++ scripts/card.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Жираф',
+    link: 'https://images.unsplash.com/photo-1678475858196-d6d9dcb3df54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+  },
+  {
+    name: 'Котик',
+    link: 'https://images.unsplash.com/photo-1678827843845-d9d6d5b80f53?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Рыжий кот',
+    link: 'https://images.unsplash.com/photo-1668174206552-cc53001e480b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80'
+  },
+  {
+    name: 'Коровы',
+    link: 'https://images.unsplash.com/photo-1500595046743-cd271d694d30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80'
+  },
+  {
+    name: 'Два слона',
+    link: 'https://images.unsplash.com/photo-1564760055775-d63b17a55c44?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1477&q=80'
+  },
+  {
+    name: 'Гиены',
+    link: 'https://images.unsplash.com/photo-1499678481508-e9f03108304b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -17,7 +17,36 @@ const nameInput = document.querySelector('#nameInput');
 const jobInput = document.querySelector('#jobInput');
 const titleInput = document.querySelector('#titleInput');
 const linkInput = document.querySelector('#linkInput');
-const templateElement = document.querySelector('#cardTemplate').content;
+
+const ulElement= document.querySelector('.elements__container');
+const cardElement = document.querySelector('#cardTemplate').content;
+
+const initialCards = [
+  {
+    name: 'Жираф',
+    link: 'https://images.unsplash.com/photo-1678475858196-d6d9dcb3df54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+  },
+  {
+    name: 'Котик',
+    link: 'https://images.unsplash.com/photo-1678827843845-d9d6d5b80f53?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Рыжий кот',
+    link: 'https://images.unsplash.com/photo-1668174206552-cc53001e480b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80'
+  },
+  {
+    name: 'Коровы',
+    link: 'https://images.unsplash.com/photo-1500595046743-cd271d694d30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80'
+  },
+  {
+    name: 'Два слона',
+    link: 'https://images.unsplash.com/photo-1564760055775-d63b17a55c44?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1477&q=80'
+  },
+  {
+    name: 'Гиены',
+    link: 'https://images.unsplash.com/photo-1499678481508-e9f03108304b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  }
+];
 
 function openPopup (popup) {
   popup.classList.add('popup_opened');
@@ -53,7 +82,29 @@ popupCloseButtonElement.forEach((element) => {
 });
 });
 
-;
+
+
+function createCard(object) {
+ const listElement = cardElement.querySelector('.elements__card-container').cloneNode(true);
+const trashElement = listElement.querySelector('.elements__delete-button');
+const photoElement = listElement.querySelector('.elements__photo');
+const likeButtonElement = listElement.querySelector('.elements__like-button');
+const titleElement = listElement.querySelector('.elements__title');
+
+ photoElement.src = object.link;
+ photoElement.alt = object.name;
+ titleElement.textContent = object.name;
+
+ return listElement;
+};
+
+initialCards.forEach((element) =>{
+  const card = createCard(element);
+  ulElement.append (card);
+
+});
+
+
 profilePopupElement.addEventListener('submit', handleFormSubmit);
 cardPopupElement.addEventListener('submit', handleFormSubmit);
 
