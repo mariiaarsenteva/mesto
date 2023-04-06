f276497889f5ca762636f373e92a43c764157b9b
Mariia Arsenteva <mariiaarsenteva@yandex.ru>
2023-04-06 17:54:50 +0300

    открываются и закрываются два попапа

--- scripts/index.js
+++ scripts/index.js
@@ -7,36 +7,53 @@ const cardPopupElement = document.querySelector('.card-popup');
 const imagePopupElement = document.querySelector('.image-popup');
 
 const popupEditButtonElement = profileElement.querySelector('.profile__edit-button');
-const popupCloseButtonElement = document.querySelector('.popup__close-button');
+const popupCloseButtonElement = document.querySelectorAll('.popup__close-button');
 const popupAddButtonElement = document.querySelector('.profile__add-button');
 const profileName = profileElement.querySelector('.profile__name');
 const profileJob = profileElement.querySelector('.profile__job');
+const popupTitle = document.querySelector('.figure__caption');
+const popupLink = document.querySelector('.figure__image');
 const nameInput = document.querySelector('#nameInput');
 const jobInput = document.querySelector('#jobInput');
 const titleInput = document.querySelector('#titleInput');
 const linkInput = document.querySelector('#linkInput');
 const templateElement = document.querySelector('#cardTemplate').content;
 
-
-
-const openPopup = function (popup) {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-    popupElement.classList.add('popup_opened');
+function openPopup (popup) {
+  popup.classList.add('popup_opened');
 };
 
-const closePopup = function (popup) {
-  popupElement.classList.remove('popup_opened');
+function closePopup (popup) {
+  popup.classList.remove('popup_opened');
 };
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closePopup();
+  closePopup(popupElement);
 }
-profilePopupElement.addEventListener('submit', handleFormSubmit);
-popupEditButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
 
+popupEditButtonElement.addEventListener('click', () => {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+   openPopup (profilePopupElement);
+});
+
+popupAddButtonElement.addEventListener('click', () => {
+  titleInput.value = popupTitle.textContent;
+  linkInput.value = popupLink.textContent;
+  openPopup(cardPopupElement);
+});
+
+popupCloseButtonElement.forEach((element) => {
+  const popupElement = element.closest('.popup');
+  element.addEventListener('click', () =>{
+  closePopup(popupElement)
+});
+});
+
+;
+profilePopupElement.addEventListener('submit', handleFormSubmit);
+cardPopupElement.addEventListener('submit', handleFormSubmit);
 
