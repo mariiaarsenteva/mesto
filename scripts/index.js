6db41003f866ff062d2ac6141e548f44f35bf0dd
Mariia Arsenteva <mariiaarsenteva@yandex.ru>
2023-04-06 14:43:37 +0300

    прописала стили для попапа с картинкой

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
 const popupElement = document.querySelector('.popup');
 const profileElement = document.querySelector('.profile');
 const profilePopupElement = document.querySelector('.profile-popup');
+const formEditProfileElement = document.querySelector('.popup__form');
+const formAddCardElement = document.querySelector('.popup__form');
 const cardPopupElement = document.querySelector('.card-popup');
 const imagePopupElement = document.querySelector('.image-popup');
-const formEditProfileElement = document.querySelector('.popup__form');
-const formAddCardElement = document.querySelectorAll('.popup__form');
+
 const popupEditButtonElement = profileElement.querySelector('.profile__edit-button');
 const popupCloseButtonElement = document.querySelector('.popup__close-button');
-const popupAddButtonElement = imagePopupElement.querySelector('.profile__add-button');
+const popupAddButtonElement = document.querySelector('.profile__add-button');
 const profileName = profileElement.querySelector('.profile__name');
 const profileJob = profileElement.querySelector('.profile__job');
 const nameInput = document.querySelector('#nameInput');
-const titleInput = document.querySelector('#titleInput');
 const jobInput = document.querySelector('#jobInput');
+const titleInput = document.querySelector('#titleInput');
 const linkInput = document.querySelector('#linkInput');
-
-const cardTemplate = document.querySelector('#cardTemplate').content;
+const templateElement = document.querySelector('#cardTemplate').content;
 
 
 
@@ -38,3 +38,5 @@ function handleFormSubmit(evt) {
 profilePopupElement.addEventListener('submit', handleFormSubmit);
 popupEditButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
+
+
