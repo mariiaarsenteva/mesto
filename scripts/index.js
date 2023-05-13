edb6ae7c5b2c533a70a616317d6f78388e4bc4b3
Mariia Arsenteva <mariiaarsenteva@yandex.ru>
2023-05-13 20:37:25 +0300

    7пр

--- scripts/index.js
+++ scripts/index.js
@@ -24,6 +24,20 @@ const linkInput = document.querySelector("#linkInput");
 const cardList = document.querySelector(".elements__container");
 const selectorTemplate = "#cardTemplate"
 
+
+const formPersonalDataElement = document.forms.personalData
+// переменая с объектом для валидации
+const validationConfig = {
+  formSelector: ".popup__form", // все формы в документе
+  inputSelector: ".popup__input", // inputList
+  errorSelectorTemplate: ".popup__error_", // шаблон для разных инпутов
+  submitButtonSelector: ".popup__submit-button", //button
+
+  disabledButtonClass: "popup__submit-button_disabled", // button disabled
+  inputErrorClass: "popup__input_invalid", // input
+  textErrorClass: "popup__error_visible", // span
+};
+
 // функция открытия попапа
 function openPopup(popup) {
   popup.classList.add("popup_opened");
@@ -112,3 +126,167 @@ popupCloseButtonElements.forEach((element) => {
     closePopup(popupElement);
   });
 });
+
+
+
+//
+//
+//
+
+
+
+class FormValidator{
+  constructor(config,form){
+    this._form = form;
+    this._inputSelector = config.inputSelector;
+    this._errorSelectorTemplate = config.errorSelectorTemplate;
+    this._submitButtonSelector = config.submitButtonSelector;
+    this._disabledButtonClass = config.disabledButtonClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._textErrorClass = config.textErrorClass;
+    this._button = this._form.querySelector(this._submitButtonSelector);
+    this._inputList = this._form.querySelectorAll(this._inputSelector);
+
+  }
+
+  _showInputError(errorTextElement, input){
+      input.classList.add(this._inputErrorClass);
+      errorTextElement.textContent = input.validationMessage;
+      errorTextElement.classList.add(this._textErrorClass)
+
+  }
+
+  _hideInputError(errorTextElement, input){
+    input.classList.remove(this._inputErrorClass);
+      errorTextElement.textContent = '';
+      errorTextElement.classList.remove(this._textErrorClass)
+  }
+
+  _hasInvalidInput(){
+
+    return Array.from(this._inputList).some((input) => !input.validity.valid);
+  }
+
+  _toggleButton(){
+   this._hasInvalidInput ? this._disabledButton() : this._enabledButton()
+
+  }
+
+  _enabledButton() {
+    this._button.classList.remove(this._disabledButtonClass);
+    this._button.removeAttribute('disabled');
+  }
+
+  _disabledButton(){_enabledButton
+    this._button.classList.add(this._disabledButtonClass);
+    this._button.setAttribute('disabled',true);
+
+  }
+
+  _checkInputValidity(input){
+    const errorTextElement = document.querySelector(`${this._errorSelectorTemplate}${input.name}`)
+    input.validity.valid ? this._hideInputError(errorTextElement, input) : this._showInputError(errorTextElement, input)
+
+  }
+
+  _setEventListener(){
+    this._inputList.forEach((input) => {
+      input.addEventListener("input", () => {
+        this._checkInputValidity(input);
+        this._toggleButton()
+
+      });
+    })
+  }
+
+
+  enableValidation(){
+
+    this._setEventListener();
+  };
+}
+
+//создаем экземпляр класса FormValidator для попапа редактирования и запускаем валидации
+const formProfileInfoValidator = new FormValidator(validationConfig, formEditProfileElement);
+formProfileInfoValidator.enableValidation();
+
+//создаем экземпляр класса FormValidator для попапа добавления карточки и запускаем валидации
+const formAddCardValidator = new FormValidator(validationConfig, formAddCardElement);
+formAddCardValidator.enableValidation();
+
+
+
+//включила влидацию
+// function enableValidation ({ formSelector, ...rest }) {
+//   const forms = Array.from(document.querySelectorAll(formSelector));
+//   forms.forEach((form) => {
+//     form.addEventListener("submit", (evt) => {
+//       evt.preventDefault();
+//     });
+//     setEventListeners(form, rest);
+//   });
+// }
+
+//утановила слушатель
+// function setEventListeners(form, { inputSelector, submitButtonSelector, ...rest }) {
+//   const formInputs = Array.from(form.querySelectorAll(inputSelector));
+//   const formButton = form.querySelector(submitButtonSelector);
+//   formInputs.forEach((input) => {
+//     disabledButton(formButton, rest);
+//     input.addEventListener("input", () => {
+//       checkInputValidity(form, input, rest);
+//       if (hasInvalidInput(formInputs)) {
+//         disabledButton(formButton, rest)
+//       } else {
+//         enabledButton(formButton, rest)
+//       }
+//     });
+//     form.addEventListener('reset', () => {
+//       disabledButton(formButton, rest)
+//     });
+//   });
+// };
+
+//проверяю валидацию
+// const checkInputValidity = (form, input, { ...rest }) => {
+//   if (input.validity.valid) {
+//     hideInputError(form, input, rest);
+//   } else {
+//     showInputError(form, input, input.validationMessage, rest);
+//   }
+// };
+
+// const hasInvalidInput = (formInputs) => {
+//   return Array.from(formInputs).some((input) => !input.validity.valid);
+// };
+
+
+
+// //скрываю ошибку
+// function hideInputError(form, input, { inputErrorClass, textErrorClass, errorSelectorTemplate }) {
+//   const error = form.querySelector(`${errorSelectorTemplate}${input.name}`)
+//   input.classList.remove(inputErrorClass);
+//   error.textContent = '';
+//   error.classList.remove(textErrorClass);
+// }
+
+// //показываю ошибкуform, input,
+// function showInputError(form, input, errorMessage, { inputErrorClass, textErrorClass, errorSelectorTemplate }) {
+//   const error = form.querySelector(`${errorSelectorTemplate}${input.name}`)
+//   input.classList.add(inputErrorClass);
+//   error.textContent = errorMessage;
+//   error.classList.add(textErrorClass);
+// }
+
+
+
+// function enabledButton(button, { disabledButtonClass }) {
+//   button.classList.remove(disabledButtonClass);
+//   button.removeAttribute('disabled');
+// }
+// function disabledButton(button, { disabledButtonClass }) {
+//   button.classList.add(disabledButtonClass);
+//   button.setAttribute('disabled', true);
+// }
+
+// enableValidation(validationConfig);
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,25 +1,57 @@
+class FormValidator{
+  constructor(config,form){
+    this._inputSelector = config.inputSelector;
+    this._errorSelectorTemplate = config._errorSelectorTemplate;
+    this._submitButtonSelector = config.submitBttonSelector;
+    this._disabledButtonClass = config.disabledButtonClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._textErrorClass = config.textErrorClass;
+    this._form = form;
+  }
 
-const validationConfig = {
-  formSelector: ".popup__form", // все формы в документе
-  inputSelector: ".popup__input", // inputList
-  errorSelectorTemplate: ".popup__error_", // шаблон для разных инпутов
-  submitButtonSelector: ".popup__submit-button", //button
+  _showInputError(errorTextElement, input){
+      input.classList.add(this._inputErrorClass);
+      errorTextElement.textContent = input.validationMessage;
+    }
 
-  disabledButtonClass: "popup__submit-button_disabled", // button disabled
-  inputErrorClass: "popup__input_invalid", // input
-  textErrorClass: "popup__error_visible", // span
-};
+  _hideInputError(errorTextElement, input){
+    input.classList.remove(this._inputErrorClass);
+      errorTextElement.textContent = '';
+  }
+
+  _checkInputValidity(){
+    const errorTextElement = this._form.querySelector((`${this._errorSelectorTemplate}${input.name}`))
+    input.validity.valid ?
+          this._hideInputError(errorTextElement, input) : this._showInputError(errorTextElement,input)
+
+  }
+
+  _setEventListener(){
+    this._inputList.forEach(input =>
+      input.addEventListener("input", () => {
+        this._checkInputValidity(input);
+        this._enabledButton()
+      })
+    )}
+
+
+  enableValidation(){
+    this._button - form.querySelector(this._submitButtonSelector);
+    this._inputList = form.querySelector(this._inputSelector);
+    this._setEventListener();
+  };
+}
 
 //включила влидацию
-function enableValidation({ formSelector, ...rest }) {
-  const forms = Array.from(document.querySelectorAll(formSelector));
-  forms.forEach((form) => {
-    form.addEventListener("submit", (evt) => {
-      evt.preventDefault();
-    });
-    setEventListeners(form, rest);
-  });
-}
+// function enableValidation ({ formSelector, ...rest }) {
+//   const forms = Array.from(document.querySelectorAll(formSelector));
+//   forms.forEach((form) => {
+//     form.addEventListener("submit", (evt) => {
+//       evt.preventDefault();
+//     });
+//     setEventListeners(form, rest);
+//   });
+// }
 
 //утановила слушатель
 function setEventListeners(form, { inputSelector, submitButtonSelector, ...rest }) {
@@ -42,45 +74,45 @@ function setEventListeners(form, { inputSelector, submitButtonSelector, ...rest 
 };
 
 //проверяю валидацию
-const checkInputValidity = (form, input, { ...rest }) => {
-  if (input.validity.valid) {
-    hideInputError(form, input, rest);
-  } else {
-    showInputError(form, input, input.validationMessage, rest);
-  }
-};
+// const checkInputValidity = (form, input, { ...rest }) => {
+//   if (input.validity.valid) {
+//     hideInputError(form, input, rest);
+//   } else {
+//     showInputError(form, input, input.validationMessage, rest);
+//   }
+// };
 
-const hasInvalidInput = (formInputs) => {
-  return Array.from(formInputs).some((input) => !input.validity.valid);
-};
-
-
-
-//скрываю ошибку
-function hideInputError(form, input, { inputErrorClass, textErrorClass, errorSelectorTemplate }) {
-  const error = form.querySelector(`${errorSelectorTemplate}${input.name}`)
-  input.classList.remove(inputErrorClass);
-  error.textContent = '';
-  error.classList.remove(textErrorClass);
-}
-
-//показываю ошибкуform, input,
-function showInputError(form, input, errorMessage, { inputErrorClass, textErrorClass, errorSelectorTemplate }) {
-  const error = form.querySelector(`${errorSelectorTemplate}${input.name}`)
-  input.classList.add(inputErrorClass);
-  error.textContent = errorMessage;
-  error.classList.add(textErrorClass);
-}
+// const hasInvalidInput = (formInputs) => {
+//   return Array.from(formInputs).some((input) => !input.validity.valid);
+// };
 
 
 
-function enabledButton(button, { disabledButtonClass }) {
-  button.classList.remove(disabledButtonClass);
-  button.removeAttribute('disabled');
-}
-function disabledButton(button, { disabledButtonClass }) {
-  button.classList.add(disabledButtonClass);
-  button.setAttribute('disabled', true);
-}
+// //скрываю ошибку
+// function hideInputError(form, input, { inputErrorClass, textErrorClass, errorSelectorTemplate }) {
+//   const error = form.querySelector(`${errorSelectorTemplate}${input.name}`)
+//   input.classList.remove(inputErrorClass);
+//   error.textContent = '';
+//   error.classList.remove(textErrorClass);
+// }
+
+// //показываю ошибкуform, input,
+// function showInputError(form, input, errorMessage, { inputErrorClass, textErrorClass, errorSelectorTemplate }) {
+//   const error = form.querySelector(`${errorSelectorTemplate}${input.name}`)
+//   input.classList.add(inputErrorClass);
+//   error.textContent = errorMessage;
+//   error.classList.add(textErrorClass);
+// }
+
+
+
+// function enabledButton(button, { disabledButtonClass }) {
+//   button.classList.remove(disabledButtonClass);
+//   button.removeAttribute('disabled');
+// }
+// function disabledButton(button, { disabledButtonClass }) {
+//   button.classList.add(disabledButtonClass);
+//   button.setAttribute('disabled', true);
+// }
 
 enableValidation(validationConfig);
