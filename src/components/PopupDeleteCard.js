2cf9b39f14e65244d5a6085e4e6206c2515a38f5
Mariia Arsenteva <mariiaarsenteva@yandex.ru>
2023-06-04 11:25:34 +0300

    9пр

--- src/components/PopupDeleteCard.js
+++ src/components/PopupDeleteCard.js
@@ -11,7 +11,7 @@ export default class PopupDeleteCard extends Popup {
     super.setEventListeners();
     this._form.addEventListener('submit', (evt) => {
       evt.preventDefault();
-      this.submitButton.textContent = `${this.submitButton.textContent}...`
+      this.submitButton.textContent = `${this.submitButton.textContent}...`;
       this._submitFunction({ card: this._element, cardId: this._cardId });
     })
   }
